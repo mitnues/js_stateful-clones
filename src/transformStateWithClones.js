0e88cd26f732e42b4aa2194234aa66db5b1070b4
milton <mitnunes.pro.com>
2025-07-08 15:33:18 -0300

    frst try

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,40 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const newState = {};
+        Object.assign(newState, ...state);
+    for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        addProperties(newState, action.extraData);
+        break;
+
+      case 'removeProperties':
+        removeProperties(newState, action.keysToRemove);
+        break;
+
+      case 'clear':
+        clearProperties(newState);
+        break;
+    }
+  }
+  return  newState;
+}
+
+function addProperties(state, extraData) {
+  Object.assign(newState, extraData);
+}
+
+function removeProperties(newState, keysToRemove) {
+  for (const key of keysToRemove) {
+    delete newState[key];
+  }
+}
+
+function clearProperties(state) {
+  for (const key in newState) {
+    delete newState[key];
+  }
 }
 
 module.exports = transformStateWithClones;
